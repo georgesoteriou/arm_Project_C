56b0bf37b2bee68255215e0e3e11889595582962
Iulia Ivana <imi17@imperial.ac.uk>
2018-05-24 16:14:11 +0100

    Implemented data structure, and pipeline skeleton

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,75 @@
 #include <stdlib.h>
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include "Data_Processing.h"
+#include "Multiply.h"
+#include "Single_Data_Transfer.h"
+#include "Branch.h"
+
+struct ARM {
+  int32_t registers[17];
+  int32_t memory[2048];
+};
 
 int main(int argc, char **argv) {
+
+  //initialise ARM to 0
+  struct ARM arm;
+  for(int i = 0; i < 17; i++){
+      arm.registers[i] = 0;
+  }
+
+  for(int i = 0; i < 2048; i++){
+      arm.memory[i] = 0;
+  }
+
+  assert(argc == 2);
+
+  //read from file & populate memory
+  FILE* code;
+  code = fopen(argv[1], "r");
+
+  if(code == NULL) {
+    fprintf(stderr, "Can't open input file!\n");
+    exit(-1);
+  }
+
+  int counter = 0;
+  while(!feof(code)) {
+    int a = fgetc(code);
+    if(a == -1) {
+      break;
+    }
+
+    arm.memory[counter] = a;
+    for(int i = 0; i < 3; i++) {
+      arm.memory[counter] <<= 8;
+      arm.memory[counter] += fgetc(code);
+    }
+
+    counter++;
+  }
+  
+  int pc = arm.registers[15];
+  while(arm.memory[pc] != 0) {
+    int command = arm.memory[pc];
+    int b27 = (1 << 27) & command;
+    int b26 = (1 << 26) & command;
+    int b25 = (1 << 25) & command;
+    int b4  = (1 << 4)  & command;
+
+    if(b27 == 1) {
+      //Branch
+    } else if(b26 == 1) {
+      //Single Data Transfer
+    } else if(b25 == 0 && b4 == 1) {
+      //Multiply
+    } else {
+      //Data Processing
+    }
+  }
+
+
   return EXIT_SUCCESS;
 }
