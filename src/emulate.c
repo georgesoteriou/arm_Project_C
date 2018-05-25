5642addafa2089884d3c1cb9936e49b068de4eb3
gs2617 <gs2617@ic.ac.uk>
2018-05-25 15:57:39 +0100

    Comments on emulate.c

--- src/emulate.c
+++ src/emulate.c
@@ -9,6 +9,7 @@
 #include "Single_Data_Transfer.h"
 #include "Branch.h"
 
+//------------EXECUTE FUNCTIONS-------------
 //function to execute correct command
 typedef int (*execution)(void);
 
@@ -27,44 +28,56 @@ void execute_fn(int execute) {
   }
 } 
 
+//------------COND FUNCTIONS-------------
 //Functions to check cond
-int eq(int n, int z, int v) {
+int eq(int32_t n, int32_t z, int32_t v) {
   return ( (z != 0) ? 1 : 0 );
 }
-int ne(int n, int z, int v) {
+int ne(int32_t n, int32_t z, int32_t v) {
   return ( (z == 0) ? 1 : 0 );
 }
-int ge(int n, int z, int v ){
+int ge(int32_t n, int32_t z, int32_t v ) {
   return ( (n == v) ? 1 : 0 );
 }
-int lt(int n, int z, int v) {
+int lt(int32_t n, int32_t z, int32_t v) {
   return ( (n != v) ? 1 : 0 );
 }
-int gt(int n, int z, int v) {
+int gt(int32_t n, int32_t z, int32_t v) {
   return (((z == 0) && (n == v)) ? 1 : 0 );
 }
-int le(int n, int z, int v) {
+int le(int32_t n, int32_t z, int32_t v) {
   return (((z != 0) || (n != v)) ? 1 : 0 );
 }
-int al(int n, int z, int v) {
+int al(int32_t n, int32_t z, int32_t v) {
   return 1;
 }
 
 //cond function table declared
-typedef int (*cond)(int, int, int);
+typedef int (*cond)(int32_t, int32_t, int32_t);
 static cond cond_table[15];
 
+
+//-------------------DECODE FUNCTION------------------
 int decode_fn(int32_t command) {
     if(command == 0) return -2;
 
-    int b27 = (1 << 27) & command;
-    int b26 = (1 << 26) & command;
-    int b25 = (1 << 25) & command;
-    int b4  = (1 << 7)  & command;
-    int b7  = (1 << 4)  & command;
-    int n   = (1 << 31) & arm.registers[16];
-    int z   = (1 << 30) & arm.registers[16];
-    int v   = (1 << 28) & arm.registers[16];
+    int32_t b27 = (1 << 27) & command;
+    int32_t b26 = (1 << 26) & command;
+    int32_t b25 = (1 << 25) & command;
+    int32_t b4  = (1 << 7)  & command;
+    int32_t b7  = (1 << 4)  & command;
+    int32_t n   = (1 << 31) & arm.registers[16];
+    int32_t z   = (1 << 30) & arm.registers[16];
+    int32_t v   = (1 << 28) & arm.registers[16];
+
+    //cond table init
+    cond_table[0]  = eq;
+    cond_table[1]  = ne;
+    cond_table[10] = ge;
+    cond_table[11] = lt;
+    cond_table[12] = gt;
+    cond_table[13] = le;
+    cond_table[14] = al;
 
     int cond = (0b1111 << 28) & command;
     int flag = cond_table[cond](n,z,v);
@@ -84,27 +97,17 @@ int decode_fn(int32_t command) {
 }
 
 int main(int argc, char **argv) {
-  //cond table init
-  cond_table[0]  = eq;
-  cond_table[1]  = ne;
-  cond_table[10] = ge;
-  cond_table[11] = lt;
-  cond_table[12] = gt;
-  cond_table[13] = le;
-  cond_table[14] = al;
-
   //initialise ARM to 0
   for(int i = 0; i < 17; i++){
       arm.registers[i] = 0;
   }
-
   for(int i = 0; i < 2048; i++){
       arm.memory[i] = 0;
   }
 
   assert(argc == 2);
 
-  //read from file & populate memory
+  //read from file
   FILE* code;
   code = fopen(argv[1], "r");
 
@@ -113,6 +116,7 @@ int main(int argc, char **argv) {
     exit(-1);
   }
 
+  //Load program to memmory
   int counter = 0;
   while(!feof(code)) {
     int a = fgetc(code);
@@ -145,6 +149,8 @@ int main(int argc, char **argv) {
     decode = fetch;
     fetch = arm.memory[++(*pc)];
   }
+  //end of loop output
+  printf("DONE");
 
   return EXIT_SUCCESS;
 }
