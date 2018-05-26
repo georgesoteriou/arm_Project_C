50aba4ed4a3ede0dea633b5e75568664b4df735e
root <root@LAPTOP-P43CFMSK.localdomain>
2018-05-26 12:47:07 +0100

    Implemented the Multiply function

--- src/Multiply.c
+++ src/Multiply.c
@@ -1,4 +1,4 @@
-#include <stdint32_t.h>
+#include <stdint.h>
 #include "Multiply.h"
 #include "global.h"
 
@@ -7,21 +7,23 @@ void multiply() {
   int32_t command = arm.memory[arm.registers[15]];
   int32_t bitA = (1 << 21) & command;
   int32_t bitS = (1 << 20) & command;
-//  int32_t rd = arm.registers[(command >> 16) & 0b1111];
+  int32_t *rd = &arm.registers[(command >> 16) & 0b1111];
   int32_t rs = arm.registers[(command >> 8) & ob1111];
   int32_t rm = arm.registers[command & ob1111];
+  int32_t result;
 
   if (bitA) {
     //multiply and accumulate
     int32_t rn = arm.registers[(command >> 12) & ob1111];
-    
+    result = rm * rs + rn;
   } else {
     //just multiply
-    
+    result = rm * rs;
   }
+  *rd = result;
 
   if (bitS) {
-    //update N and Z
+    flagsZN(result);
   }
 
 }
