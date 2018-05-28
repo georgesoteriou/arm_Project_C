b9a8a3d4534b7ad087059fb1c4cecf213e7dd629
Iulia Ivana <imi17@imperial.ac.uk>
2018-05-28 20:00:52 +0100

    Fixed printing again

--- src/emulate.c
+++ src/emulate.c
@@ -63,14 +63,14 @@ int main(int argc, char **argv) {
 
   for(int i = 0; i < 17; i++) {
     if(i == 15) {
-      printf("PC  :% 11i (%08x)", 4 * arm.registers[i], arm.registers[i]);
+      printf("PC  :% 11i (0x%08x)", 4 * arm.registers[i], arm.registers[i]);
       printf("\n");
       
     } else if(i == 16) {
-      printf("CPSR:% 11i (%08x)", arm.registers[i], arm.registers[i]);
+      printf("CPSR:% 11i (0x%08x)", arm.registers[i], arm.registers[i]);
       printf("\n");
     } else if(i != 13 && i != 14) {
-      printf("$%-3i:% 11i (%08x)", i, arm.registers[i], arm.registers[i]);
+      printf("$%-3i:% 11i (0x%08x)", i, arm.registers[i], arm.registers[i]);
       printf("\n");
     }
   }
