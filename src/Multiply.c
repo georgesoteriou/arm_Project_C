fdc0fd3848e810cfbbdff67bec8a828b92b2e878
Iulia Ivana <imi17@imperial.ac.uk>
2018-05-28 12:34:37 +0100

    Fixed gcc extension not working with binary

--- src/Multiply.c
+++ src/Multiply.c
@@ -7,14 +7,14 @@ void multiply() {
   int32_t command = executeCommand;
   int32_t bitA = (1 << 21) & command;
   int32_t bitS = (1 << 20) & command;
-  int32_t *rd = &arm.registers[(command >> 16) & 0b1111];
-  int32_t rs = arm.registers[(command >> 8) & 0b1111];
-  int32_t rm = arm.registers[command & 0b1111];
+  int32_t *rd = &arm.registers[(command >> 16) & ((1 << 4) - 1)];
+  int32_t rs = arm.registers[(command >> 8) & ((1 << 4) - 1)];
+  int32_t rm = arm.registers[command & ((1 << 4) - 1)];
   int32_t result;
 
   if (bitA) {
     //multiply and accumulate
-    int32_t rn = arm.registers[(command >> 12) & 0b1111];
+    int32_t rn = arm.registers[(command >> 12) & ((1 << 4) - 1)];
     result = rm * rs + rn;
   } else {
     //just multiply
