6b5b14863a8943ddbb42e3bf8276a7304b57729e
Iulia Ivana <imi17@imperial.ac.uk>
2018-05-28 12:10:00 +0100

    Fixed bugs and used executeCommand instread of the current command

--- src/Multiply.c
+++ src/Multiply.c
@@ -4,17 +4,17 @@
 
 void multiply() {
   //memory[registers[15]] = instructiunea mea
-  int32_t command = arm.memory[arm.registers[15]];
+  int32_t command = executeCommand;
   int32_t bitA = (1 << 21) & command;
   int32_t bitS = (1 << 20) & command;
   int32_t *rd = &arm.registers[(command >> 16) & 0b1111];
-  int32_t rs = arm.registers[(command >> 8) & ob1111];
-  int32_t rm = arm.registers[command & ob1111];
+  int32_t rs = arm.registers[(command >> 8) & 0b1111];
+  int32_t rm = arm.registers[command & 0b1111];
   int32_t result;
 
   if (bitA) {
     //multiply and accumulate
-    int32_t rn = arm.registers[(command >> 12) & ob1111];
+    int32_t rn = arm.registers[(command >> 12) & 0b1111];
     result = rm * rs + rn;
   } else {
     //just multiply
